156929a9e0b7797dd792efee4ece1001580f26b0
CodingSecurely <144389450+CodingSecurely@users.noreply.github.com>
2023-09-07 22:21:58 +0100

    Update Files

--- Encryption.py
+++ Encryption.py
@@ -1,5 +1,9 @@
 import random
 import string
 
-chars = string.punctuation + string.digits + string.ascii_letters
-print (chars)
+chars = " "+ string.punctuation + string.digits + string.ascii_letters
+chars = list(chars)
+key = chars.copy()
+random.shuffle(key)
+print (f"chars: {chars}")
+print (f"keys: {key}")
