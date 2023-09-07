457ee320f39b8c9a822ccd8645b308e18f69aafb
MAsecurity <108958617+MAsecurity@users.noreply.github.com>
2023-09-07 22:36:58 +0100

    Add Encryption method

--- Encryption.py
+++ Encryption.py
@@ -5,5 +5,15 @@ chars = " "+ string.punctuation + string.digits + string.ascii_letters
 chars = list(chars)
 key = chars.copy()
 random.shuffle(key)
-print (f"chars: {chars}")
-print (f"keys: {key}")
+#Encrypt
+plain_text = input("Enter a message: ")
+cipher_text = ""
+for letter in plain_text:
+    index = chars.index(letter)
+    cipher_text += key[index]
+
+print(f"Original message: {plain_text}")
+print(f"Encrypted text: {cipher_text}")
+
+
+
